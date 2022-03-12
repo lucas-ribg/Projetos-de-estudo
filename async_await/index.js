61ec0b37e1a619cde620e8c432aa8c39d46bf7cb
Lucas Ribeiro <lucas123.r.g@gmail.com>
2022-03-12 13:08:59 -0300

    get

--- async_await/index.js
+++ async_await/index.js
@@ -1,7 +1,30 @@
 require('dotenv').config()
 
+const axios = require('axios')
+
 const {PROTOCOL, BASE_URL, APPID, LANGUAGE, UNITS, Q} = process.env
 
 const url = `${PROTOCOL}://${BASE_URL}?appid=${APPID}&lang=${LANGUAGE}&units=${UNITS}&q=${Q}`
 
-console.log(url)
+axios.get(url)
+.then(res => {
+    return res.data
+})
+.then(res => {
+    console.log(res.cnt)
+    return res
+})
+.then(res => {
+    return res.list
+})
+.then(res => {
+    for (let previsao of res) {
+        console.log(`
+            ${new Date(+previsao.dt * 1000).toLocaleString()},
+            Min: ${previsao.main.temp_min}\u00B0C,
+            Max: ${previsao.main.temp_max}\u00B0C,
+            Hum: ${previsao.main.humidity}%,
+            ${previsao.weather[0].description}
+        `)
+    }
+})
