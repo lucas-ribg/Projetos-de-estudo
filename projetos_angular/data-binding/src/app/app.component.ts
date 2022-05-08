6bf911c8f277d383e665a200741747985e82c7d4
Lucas Ribeiro <lucas123.r.g@gmail.com>
2022-05-08 17:15:37 -0300

    att

---
+++ projetos_angular/data-binding/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+
+  numero:number
+
+  nome:string
+  esconderCaixa = true
+
+  escolher(): void{
+    this.numero = Math.floor(Math.random() * 100) + 1
+  }
+  
+  adicionar(nomeInput): void {
+    this.nome = nomeInput.value
+    this.esconderCaixa = false
+    // console.log(nomeInput)
+    // console.log('Adicionando...')
+  }
+
+  // alterarNome(event) {
+  //   console.log(event.target.value)
+  //   this.nome = event
+  // }
+  
+  // lancarDado(): number {
+  //   return Math.floor(Math.random() * 6) + 1
+  // }
+}
